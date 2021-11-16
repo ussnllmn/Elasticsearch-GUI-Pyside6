fc079b1cdfff1670fcc5d48a27860601fb3598c8
Kridsanapong Buathongjun <69898533+ussnllmn@users.noreply.github.com>
2021-11-16 16:00:41 +0700

    Add modern gui

---
+++ GUI_Elasticsearch/main.py
@@ -0,0 +1,234 @@
+# ///////////////////////////////////////////////////////////////
+#
+# BY: WANDERSON M.PIMENTA
+# PROJECT MADE WITH: Qt Designer and PySide6
+# V: 1.0.0
+#
+# This project can be used freely for all uses, as long as they maintain the
+# respective credits only in the Python scripts, any information in the visual
+# interface (GUI) can be modified without any implication.
+#
+# There are limitations on Qt licenses if you want to use your products
+# commercially, I recommend reading them on the official website:
+# https://doc.qt.io/qtforpython/licenses.html
+#
+# ///////////////////////////////////////////////////////////////
+
+import sys
+import os
+import platform
+import json
+
+# import the Elasticsearch client library
+from elasticsearch import Elasticsearch
+# IMPORT / GUI AND MODULES AND WIDGETS
+# ///////////////////////////////////////////////////////////////
+from modules import *
+from widgets import *
+os.environ["QT_FONT_DPI"] = "96" # FIX Problem for High DPI and Scale above 100%
+# define a function for the Elasticsearch query API call
+
+# create a client instance of Elasticsearch
+client = Elasticsearch("http://localhost:9200")
+
+def make_query(filter, index_name):
+
+    # make an API call to check if the index exists
+    index_exists = client.indices.exists(index=index_name)
+
+    # if it exists then make the API call
+    if index_exists == True:
+        print ("index_name:", index_name, "exists.")
+        print ("FILTER:", filter, "\n")
+
+        # catch any exceptions and return them to Kivy app
+        try:
+            # pass filter query to the client's search() method
+            response = client.search(index=index_name, body=filter)
+            response = client.
+            # print the query response for debugging purposes
+            print ('response["hits"]:', len(response["hits"]))
+            print ('response TYPE:', type(response))
+
+        except Exception as err:
+            print ("search() index ERROR", err)
+            response = {"error": str(err)}
+
+    # error text response if index doesn't exist
+    else:
+        # build a string for the index-does-not-exist response
+        resp_text = "Elasticsearch index name '" + str(index_name)
+        resp_text += "' does not exist."
+        response = {"response": resp_text}
+
+    # return the dict response to Kivy app
+    return response
+
+# SET AS GLOBAL WIDGETS
+# ///////////////////////////////////////////////////////////////
+widgets = None
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        # SET AS GLOBAL WIDGETS
+        # ///////////////////////////////////////////////////////////////
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        global widgets
+        widgets = self.ui
+
+        # USE CUSTOM TITLE BAR | USE AS "False" FOR MAC OR LINUX
+        # ///////////////////////////////////////////////////////////////
+        Settings.ENABLE_CUSTOM_TITLE_BAR = True
+
+        # APP NAME
+        # ///////////////////////////////////////////////////////////////
+        title = "PyDracula - Modern GUI"
+        description = "PyDracula APP - Theme with colors based on Dracula for Python."
+        # APPLY TEXTS
+        self.setWindowTitle(title)
+        widgets.titleRightInfo.setText(description)
+
+        # TOGGLE MENU
+        # ///////////////////////////////////////////////////////////////
+        widgets.toggleButton.clicked.connect(lambda: UIFunctions.toggleMenu(self, True))
+
+        # SET UI DEFINITIONS
+        # ///////////////////////////////////////////////////////////////
+        UIFunctions.uiDefinitions(self)
+
+        # QTableWidget PARAMETERS
+        # ///////////////////////////////////////////////////////////////
+
+        # BUTTONS CLICK
+        # ///////////////////////////////////////////////////////////////
+
+        # LEFT MENUS
+        widgets.btn_home.clicked.connect(self.buttonClick)
+        widgets.btn_widgets.clicked.connect(self.buttonClick)
+        widgets.btn_new.clicked.connect(self.buttonClick)
+        widgets.btn_save.clicked.connect(self.buttonClick)
+        widgets.B_make.clicked.connect(self.buttonClick)
+        # EXTRA LEFT BOX
+        def openCloseLeftBox():
+            UIFunctions.toggleLeftBox(self, True)
+        widgets.toggleLeftBox.clicked.connect(openCloseLeftBox)
+        widgets.extraCloseColumnBtn.clicked.connect(openCloseLeftBox)
+
+        # EXTRA RIGHT BOX
+        def openCloseRightBox():
+            UIFunctions.toggleRightBox(self, True)
+        widgets.settingsTopBtn.clicked.connect(openCloseRightBox)
+
+        # SHOW APP
+        # ///////////////////////////////////////////////////////////////
+        self.show()
+
+        # SET CUSTOM THEME
+        # ///////////////////////////////////////////////////////////////
+        useCustomTheme = False
+        themeFile = "themes\py_dracula_light.qss"
+
+        # SET THEME AND HACKS
+        if useCustomTheme:
+            # LOAD AND APPLY STYLE
+            UIFunctions.theme(self, themeFile, True)
+
+            # SET HACKS
+            AppFunctions.setThemeHack(self)
+
+        # SET HOME PAGE AND SELECT MENU
+        # ///////////////////////////////////////////////////////////////
+        widgets.stackedWidget.setCurrentWidget(widgets.home)
+        widgets.btn_home.setStyleSheet(UIFunctions.selectMenu(widgets.btn_home.styleSheet()))
+
+
+    # BUTTONS CLICK
+    # Post here your functions for clicked buttons
+    # ///////////////////////////////////////////////////////////////
+    def buttonClick(self):
+        # GET BUTTON CLICKED
+        btn = self.sender()
+        btnName = btn.objectName()
+
+        # SHOW HOME PAGE
+        if btnName == "btn_home":
+            widgets.stackedWidget.setCurrentWidget(widgets.home)
+            UIFunctions.resetStyle(self, btnName)
+            btn.setStyleSheet(UIFunctions.selectMenu(btn.styleSheet()))
+            # SHOW WIDGETS PAGE
+
+        if btnName == "B_make":
+            print("make")
+            # pass the field name and query args to filter dict
+            filter = {
+                'query': {
+                    'match': {
+                        self.ui.F_name.text(): self.ui.Q_name.text()
+                    }
+                }
+            }
+
+            # get the index name and put into a string variable
+            index_name = self.ui.I_name.text()
+            print("field_input", self.ui.F_name.text())
+            print("query_input", self.ui.Q_name.text())
+            print("index_input", self.ui.I_name.text())
+            # make sure that the user has entered an index name
+            # if len(index_name) == 0 or index_name == "":
+            #     json_resp = '{"error", "index name field cannot be empty"}'
+            # else:
+                # pass the filter dict to the make_query() function
+            resp = make_query(filter, index_name)
+            json_resp = json.dumps(resp, indent=4)
+            print("Elasticsearch response:", resp)
+            # json_resp = '{"error", "index name field cannot be empty"}'
+            # change the label to match the Elasticsearch API response
+            self.ui.textBrowser.setText(json_resp)
+
+
+
+        # SHOW WIDGETS PAGE
+        if btnName == "btn_widgets":
+            widgets.stackedWidget.setCurrentWidget(widgets.widgets)
+            UIFunctions.resetStyle(self, btnName)
+            btn.setStyleSheet(UIFunctions.selectMenu(btn.styleSheet()))
+
+        # SHOW NEW PAGE
+        if btnName == "btn_new":
+            widgets.stackedWidget.setCurrentWidget(widgets.new_page) # SET PAGE
+            UIFunctions.resetStyle(self, btnName) # RESET ANOTHERS BUTTONS SELECTED
+            btn.setStyleSheet(UIFunctions.selectMenu(btn.styleSheet())) # SELECT MENU
+
+        if btnName == "btn_save":
+            print("Save BTN clicked!")
+
+        # PRINT BTN NAME
+        print(f'Button "{btnName}" pressed!')
+
+
+    # RESIZE EVENTS
+    # ///////////////////////////////////////////////////////////////
+    def resizeEvent(self, event):
+        # Update Size Grips
+        UIFunctions.resize_grips(self)
+
+    # MOUSE CLICK EVENTS
+    # ///////////////////////////////////////////////////////////////
+    def mousePressEvent(self, event):
+        # SET DRAG POS WINDOW
+        self.dragPos = event.globalPos()
+
+        # PRINT MOUSE EVENTS
+        if event.buttons() == Qt.LeftButton:
+            print('Mouse click: LEFT CLICK')
+        if event.buttons() == Qt.RightButton:
+            print('Mouse click: RIGHT CLICK')
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("icon.ico"))
+    window = MainWindow()
+    sys.exit(app.exec_())
